df11436b272af854661293edfcb3b44f5d78033c
Breachfix <Ntihanirahojacques@gmail.com>
2024-09-06 18:26:36 -0700

    Updated backend server configuration

--- backend/server.js
+++ backend/server.js
@@ -59,15 +59,15 @@ function sendEmail({ recipient_email, OTP }) {
       var transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-          user: process.env.MY_EMAIL,
-          pass: process.env.MY_PASSWORD,
+          user: ENV_VARS.MY_EMAIL,
+          pass: ENV_VARS.MY_PASSWORD,
         },
       });
   
       const mail_configs = {
-        from: process.env.MY_EMAIL,
+        from: ENV_VARS.MY_EMAIL,
         to: recipient_email,
-        subject: "KODING 101 PASSWORD RECOVERY",
+        subject: "PASSWORD RECOVERY",
         html: `<!DOCTYPE html>
   <html lang="en" >
   <head>
@@ -81,17 +81,17 @@ function sendEmail({ recipient_email, OTP }) {
   <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
     <div style="margin:50px auto;width:70%;padding:20px 0">
       <div style="border-bottom:1px solid #eee">
-        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Koding 101</a>
+        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">BreachFix</a>
       </div>
       <p style="font-size:1.1em">Hi,</p>
-      <p>Thank you for choosing Koding 101. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
+      <p>Thank you for choosing BreachFix. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
       <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
-      <p style="font-size:0.9em;">Regards,<br />Koding 101</p>
+      <p style="font-size:0.9em;">Regards,<br />BreachFix</p>
       <hr style="border:none;border-top:1px solid #eee" />
       <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-        <p>Koding 101 Inc</p>
-        <p>1600 Amphitheatre Parkway</p>
-        <p>California</p>
+        <p>breachfix</p>
+        <p>Port Coquitlam</p>
+        <p>BC</p>
       </div>
     </div>
   </div>
